9ea194166b1fcf823da56df421dc8ee22f5d3d52
harshapakki <hpakki@ufl.edu>
2022-02-04 14:52:21 -0500

    Header Footer

---
+++ workspaces/client/src/footer.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+  
+const Footer = () => (
+  <footer className="footer">
+    <p>Contact Details: 911</p>
+    <p>Copyrights: Reitz Union Project Group</p>
+    <p>Ref: https://union.ufl.edu/hotel/</p>
+  </footer>
+);
+  
+export default Footer;
