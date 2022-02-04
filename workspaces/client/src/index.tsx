42db24131969ced487cd3d649fcbbcd233446571
harshapakki <hpakki@ufl.edu>
2022-02-04 14:52:03 -0500

    App and index files updated

--- workspaces/client/src/index.tsx
+++ workspaces/client/src/index.tsx
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './home';
+import Header from './header';
+import Footer from './footer';
+import Login from './login';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<App />} />
+    <Route path="/Header" element={<Header />} />
+    <Route path="/Home" element={<Home />} />
+    <Route path="/footer" element={<Footer />} />
+    <Route path="/Login" element={<Login />} />
+  </Routes>
+</BrowserRouter>,
+document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
