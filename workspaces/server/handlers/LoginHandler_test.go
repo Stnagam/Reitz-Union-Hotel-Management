ea1c2a45786bca7533143371a307432e061b33f8
sheelaippili <sheelaippili140@gmail.com>
2022-03-02 11:29:25 -0500

    unit tests for signup and login handlers

---
+++ workspaces/server/handlers/LoginHandler_test.go
@@ -0,0 +1,35 @@
+package handlers
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"server/utils"
+	"strings"
+	"testing"
+)
+
+func TestLoginHandler(t *testing.T) {
+	//use mock db database name in .env file
+	utils.DBConnection()
+
+	var jsonStr = []byte(`{"email":"newuser1@gmail.com","password":"user1@123"}`)
+
+	req, err := http.NewRequest("POST", "/login", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(LoginHandler)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	expected := `"message":"logged in"`
+	if strings.Contains(rr.Body.String(), expected) {
+	} else {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
---
+++ workspaces/server/handlers/SignUpHandler_test.go
@@ -0,0 +1,36 @@
+package handlers
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"server/utils"
+	"strings"
+	"testing"
+)
+
+func TestSignupHandler(t *testing.T) {
+	//use mock db database name in .env file
+	utils.DBConnection()
+
+	var jsonStr = []byte(`{"firstname":"New","lastname":"User3","email":"newuser3@gmail.com","password":"user3@123","mobile":1234567888,"age":12}`)
+
+	req, err := http.NewRequest("POST", "/signup", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(SignUpHandler)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := `{"message":"Success"}`
+	if strings.Contains(rr.Body.String(), expected) {
+	} else {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
