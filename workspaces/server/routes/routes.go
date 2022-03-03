9c3fa721fbbbb49d3af3c4d329989eecebc7bea5
SAI TEJA NAGAM <saitejanagam@gmail.com>
2022-03-03 12:53:59 -0500

    Added subrouter

--- workspaces/server/routes/routes.go
+++ workspaces/server/routes/routes.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"server/handlers"
 
-	//"server/utils"
+	"server/utils"
 
 	h "github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
@@ -17,7 +17,7 @@ func Controller() {
 
 	cors := h.CORS(
 		h.AllowedOrigins([]string{"http://localhost:3000"}),
-		h.AllowedHeaders([]string{"accept", "origin", "X-Requested-With", "Content-Type", "Authorization"}),
+		h.AllowedHeaders([]string{"accept", "origin", "X-Requested-With", "x-access-token", "Content-Type", "Authorization"}),
 		h.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"}),
 		h.OptionStatusCode(204),
 		h.AllowCredentials(),
@@ -25,8 +25,14 @@ func Controller() {
 
 	router.HandleFunc("/signup", handlers.SignUpHandler).Methods("POST")
 	router.HandleFunc("/login", handlers.LoginHandler).Methods("POST")
-	router.HandleFunc("/logout", handlers.LogoutHandler).Methods("POST")
+
+	s := router.PathPrefix("/auth").Subrouter()
+	s.Use(utils.JwtVerify)
+	//s.HandleFunc("/dummy", handlers.DummyHandler).Methods("POST")
+	s.HandleFunc("/logout", handlers.LogoutHandler).Methods("POST")
+	s.HandleFunc("/bookings", handlers.BookingHandler).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":8080", cors(router)))
+
 	fmt.Print("Server is Up!")
 }
