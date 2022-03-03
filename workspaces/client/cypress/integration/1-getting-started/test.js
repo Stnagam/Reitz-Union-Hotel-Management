00dd987a28a42f4af9c0735f58b965de6aa03d00
hetanshpatel <hetanshnpatel@gmail.com>
2022-03-03 17:39:17 -0500

    Cypress test for signup module + Token storage and token kill on client side

--- workspaces/client/cypress/integration/1-getting-started/test.js
+++ workspaces/client/cypress/integration/1-getting-started/test.js
@@ -4,29 +4,30 @@ describe('Test', function(){
 
     it('sign up', function(){
 
-        cy.visit('http://localhost:3000/Signup');
+        cy.visit('http://localhost:3000/');
         cy.viewport('macbook-15');
-        // cy.get('a').contains('Signup').click();
+        cy.get('button').contains('SignUp').click();
         cy.get('input[name="firstname"]').type(name());
-         cy.get('input[name="lastname"]').type(name());
+        cy.get('input[name="lastname"]').type(name());
         cy.get('input[name="email"]').type(name()+'@gmail.com');
         cy.get('input[name="password"]').type(pass(8));
         cy.get('input[name="password1"]').type(pass(8));
         cy.get('input[name="mobile"]').type('1'+numberGen(9));
         cy.get('input[name="age"]').type(numberGen(2));
         cy.get('button').contains('Register').click();
-        cy.get('a').contains('Forgot').should('have.text', 'Login');
+        cy.get('a').contains('Forgot Password?').should('have.text', 'Login');
+        // cy.get('input[name="firstname"]').;
 
 
     });
 
     function pass(i){
-        const pass = "123";
+        const pass = "12345678";
         var password = "";
          for(var j = 0; j < i; j++){
             password += pass.charAt(Math.floor(Math.random()*pass.length));
         }
-        return password;
+        return pass;
     }
 
     function numberGen(i){
--- workspaces/server/routes/routes.go
+++ workspaces/server/routes/routes.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"server/handlers"
+	"server/utils"
 
 	//"server/utils"
 
@@ -17,7 +18,7 @@ func Controller() {
 
 	cors := h.CORS(
 		h.AllowedOrigins([]string{"http://localhost:3000"}),
-		h.AllowedHeaders([]string{"accept", "origin", "X-Requested-With", "Content-Type", "Authorization"}),
+		h.AllowedHeaders([]string{"accept", "origin", "X-Requested-With", "Content-Type", "x-access-token"}),
 		h.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"}),
 		h.OptionStatusCode(204),
 		h.AllowCredentials(),
@@ -26,7 +27,11 @@ func Controller() {
 	router.HandleFunc("/signup", handlers.SignUpHandler).Methods("POST")
 	router.HandleFunc("/login", handlers.LoginHandler).Methods("POST")
 	router.HandleFunc("/logout", handlers.LogoutHandler).Methods("POST")
+	s := router.PathPrefix("/auth").Subrouter()
+	s.Use(utils.JwtVerify)
+	s.HandleFunc("/dummy", handlers.DummyHandler).Methods("POST")
 
+	cors(s)
 	log.Fatal(http.ListenAndServe(":8080", cors(router)))
 	fmt.Print("Server is Up!")
 }
