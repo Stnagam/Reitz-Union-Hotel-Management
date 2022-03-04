e47126a1ce83b4f446a75cdc3198fb90c85fb610
sheelaippili <sheelaippili140@gmail.com>
2022-03-04 18:28:20 -0500

    login handler unit test updated

--- workspaces/server/handlers/LoginHandler_test.go
+++ workspaces/server/handlers/LoginHandler_test.go
@@ -13,7 +13,7 @@ func TestLoginHandler(t *testing.T) {
 	//use mock db database name in .env file
 	utils.DBConnection()
 
-	var jsonStr = []byte(`{"email":"newuser1@gmail.com","password":"user1@123"}`)
+	var jsonStr = []byte(`{"email":"newuser2@yahoo.com","password":"newuser1"}`)
 
 	req, err := http.NewRequest("POST", "/login", bytes.NewBuffer(jsonStr))
 	if err != nil {
@@ -27,7 +27,86 @@ func TestLoginHandler(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
 	}
+
 	expected := `"message":"logged in"`
+	if strings.Contains(rr.Body.String(), expected) {
+	} else {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
+
+func TestLoginHandlerInvalidCred(t *testing.T) {
+	//use mock db database name in .env file
+	utils.DBConnection()
+
+	var jsonStr = []byte(`{"email":"sheelaipperweili14@gmail.com","password":"us1@123"}`)
+
+	req, err := http.NewRequest("POST", "/login", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(LoginHandler)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := `{"message":"Invalid login credentials. Please try again"}`
+	if strings.Contains(rr.Body.String(), expected) {
+	} else {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
+
+func TestLoginHandlerInvalidRequest(t *testing.T) {
+	//use mock db database name in .env file
+	utils.DBConnection()
+
+	var jsonStr = []byte(`{"email":"newer1@gmail.com","password":"us1@123", ""}`)
+
+	req, err := http.NewRequest("POST", "/login", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(LoginHandler)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := `{"message":"Invalid request","status":false}`
+	if strings.Contains(rr.Body.String(), expected) {
+	} else {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
+
+func TestLoginHandlerMailNotFound(t *testing.T) {
+	//use mock db database name in .env file
+	utils.DBConnection()
+
+	var jsonStr = []byte(`{"email":"neweryr1@gmail.com","password":"us1@123"}`)
+
+	req, err := http.NewRequest("POST", "/login", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(LoginHandler)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := `{"message":"Email address not found","status":false}`
 	if strings.Contains(rr.Body.String(), expected) {
 	} else {
 		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
