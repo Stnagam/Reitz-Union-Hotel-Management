48a48bcb311d791968046c9d68155d4ed6e77402
hetanshpatel <hetanshnpatel@gmail.com>
2022-03-04 00:02:27 -0500

    Booking + Payement

--- workspaces/client/src/Pages/Booking.js
+++ workspaces/client/src/Pages/Booking.js
@@ -1,18 +1,53 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
+import { useNavigate } from "react-router";
+import axios from "axios";
 import Header from "../default/header";
 import Footer from "../default/footer";
 import "../Style/booking.css";
 import Header_Common from "../components/header_common";
+import { orange } from "@mui/material/colors";
 
 const Booking = () => {
+  const navigate = useNavigate();
+
   const [filterdata, setFilterdata] = useState({
     adults: 0,
     children: 0,
     checkin: "",
     checkout: "",
   });
+
+  const [errors, seterrors] = useState([]);
+
+  const handleValidation = () => {
+    let errors = {};
+    let formIsValid = true;
+
+    if (adults < 1) {
+      formIsValid = false;
+      errors["adults"] = "Select number of adults";
+    }
+
+    // if (children < 1) {
+    //   formIsValid = false;
+    //   errors["children"] = "Select number of children";
+    // }
+
+    if (!checkin) {
+      formIsValid = false;
+      errors["checkin"] = "Select checkin date";
+    }
+
+    if (!checkout) {
+      formIsValid = false;
+      errors["checkout"] = "Select checkout date";
+    }
+
+    seterrors(errors);
+    return formIsValid;
+  };
 
   const { adults, children, checkin, checkout } = filterdata;
 
@@ -21,14 +56,21 @@ const Booking = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(filterdata);
+
+    if (handleValidation()) {
+      localStorage.setItem("adults", adults);
+      localStorage.setItem("childrens", children);
+      localStorage.setItem("checkin", checkin);
+      localStorage.setItem("checkout", checkout);
+      navigate("/payment");
+    }
   };
   return (
     <div>
       <Header_Common />
       <div className="form">
         <div className="filter">
-          <Form onSubmit={(e) => onSubmit(e)}>
+          <Form>
             <div className="checkin">
               <Form.Group controlId="checkin">
                 <h4>
@@ -39,9 +81,13 @@ const Booking = () => {
                   type="date"
                   value={checkin}
                   required
+                  style={{ width: "100%" }}
                   onChange={(e) => onChange(e)}
                 />
                 <br />
+                <span style={{ color: "red", fontSize: "15px" }}>
+                  {errors["checkin"]}
+                </span>
               </Form.Group>
             </div>
 
@@ -56,23 +102,27 @@ const Booking = () => {
                   placeholder="Enter Password"
                   value={checkout}
                   required
+                  style={{ width: "100%" }}
                   onChange={(e) => onChange(e)}
                 />
                 <br />
+                <span style={{ color: "red", fontSize: "15px" }}>
+                  {errors["checkout"]}
+                </span>
               </Form.Group>
             </div>
 
             <div className="adults">
               <h4>
-                <label for="exampleFormControlSelect1">
-                  Adults(13 and above)
-                </label>
+                <label htmlFor="adults">Adults(13 and above)</label>
               </h4>
               <select
-                class="form-control"
+                className="form-control"
                 name="adults"
+                style={{ width: "100%" }}
                 onChange={(e) => onChange(e)}
               >
+                <option>0</option>
                 <option>1</option>
                 <option>2</option>
                 <option>3</option>
@@ -82,34 +132,44 @@ const Booking = () => {
                 <option>7</option>
                 <option>8</option>
               </select>
+              <span style={{ color: "red", fontSize: "15px" }}>
+                {errors["adults"]}
+              </span>
             </div>
             <br />
 
             <div className="children">
               <h4>
-                <label for="exampleFormControlSelect1">
-                  Children(12 and below)
-                </label>
+                <label htmlFor="children">Children(12 and below)</label>
               </h4>
-              <select name="children" onChange={(e) => onChange(e)}>
+              <select
+                name="children"
+                style={{ width: "100%" }}
+                onChange={(e) => onChange(e)}
+              >
+                <option>0</option>
                 <option>1</option>
                 <option>2</option>
                 <option>3</option>
                 <option>4</option>
                 <option>5</option>
               </select>
+              <span style={{ color: "red", fontSize: "15px" }}>
+                {errors["children"]}
+              </span>
             </div>
           </Form>
         </div>
-        <Link to="/rooms">
-          <Button
-            type="submit"
-            className="booking"
-            onSubmit={(e) => onSubmit(e)}
-          >
-            Select Rooms
-          </Button>
-        </Link>
+        {/* <Link to="/rooms" state={{ filterdata }}> */}
+        <Button
+          type="submit"
+          className="booking"
+          style={{ background: "orange", width: "15%", marginTop: "20px" }}
+          onClick={(e) => onSubmit(e)}
+        >
+          <div style={{ fontSize: "15px" }}>Select Rooms</div>
+        </Button>
+        {/* </Link> */}
       </div>
       <Footer />
     </div>
