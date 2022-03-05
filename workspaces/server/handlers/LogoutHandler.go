966818fb1f884f9e392e722c4e4ab3690b1c105c
SAI TEJA NAGAM <saitejanagam@gmail.com>
2022-03-04 21:15:50 -0500

    Updated LogoutHandler with response

--- workspaces/server/handlers/LogoutHandler.go
+++ workspaces/server/handlers/LogoutHandler.go
@@ -5,16 +5,21 @@ import (
 	"net/http"
 	"server/models"
 )
-func LogoutHandler(w http.ResponseWriter, r *http.Request){
 
-	 	user := &models.User{}
-	err := json.NewDecoder(r.Body).Decode(user)
+func LogoutHandler(w http.ResponseWriter, r *http.Request) {
+
+	IncomingMessage := &models.Exception{}
+	err := json.NewDecoder(r.Body).Decode(IncomingMessage)
 	if err != nil {
 		var resp = map[string]interface{}{"status": false, "message": "Invalid request"}
 		json.NewEncoder(w).Encode(resp)
 		return
 	}
 
-	
+	if IncomingMessage.Message == "kill token" {
+		var resp = map[string]interface{}{"message": "Token Has been Blacklisted"}
+		json.NewEncoder(w).Encode(resp)
+		return
+	}
 
 }
