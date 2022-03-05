6d040b4e172a5831906dcd8c2f01f9fc0505bb72
SAI TEJA NAGAM <saitejanagam@gmail.com>
2022-03-04 21:33:30 -0500

    Testing LogoutHandler done

---
+++ workspaces/server/handlers/LogoutHandler_test.go
@@ -0,0 +1,36 @@
+package handlers
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"server/utils"
+	"strings"
+	"testing"
+)
+
+func TestLogoutHandler(t *testing.T) {
+	//use mock db database name in .env file
+	utils.DBConnection()
+
+	var jsonStr = []byte(`{"message": "kill token"}`)
+
+	req, err := http.NewRequest("POST", "/auth/logout", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(LogoutHandler)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := `{"message":"Token Has been Blacklisted"}`
+	if strings.Contains(rr.Body.String(), expected) {
+	} else {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
