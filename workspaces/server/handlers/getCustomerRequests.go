6d81b1d01061d2d7b1cc7853de824426a5e5fa81
SAI TEJA NAGAM <saitejanagam@gmail.com>
2022-03-23 15:22:06 -0400

    Added initial various get  Customer request APIs

---
+++ workspaces/server/handlers/getCustomerRequests.go
@@ -0,0 +1,38 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"server/models"
+	"server/utils"
+)
+
+func GetPendingReqs(w http.ResponseWriter, r *http.Request) {
+	var allRequests []models.CustomerRequest
+	
+
+	utils.DB.Where("status = ?", "Pending").Find(&allRequests)
+
+	json.NewEncoder(w).Encode(&allRequests)
+
+}
+
+func GetAssignedReqs(w http.ResponseWriter, r *http.Request) {
+	var allRequests []models.CustomerRequest
+
+
+	utils.DB.Where("status = ?", "Assigned").Find(&allRequests)
+
+	json.NewEncoder(w).Encode(&allRequests)
+
+}
+func GetCompletedReqs(w http.ResponseWriter, r *http.Request) {
+	var allRequests []models.CustomerRequest
+
+
+	utils.DB.Where("status = ?", "Completed").Find(&allRequests)
+
+	json.NewEncoder(w).Encode(&allRequests)
+
+}
+
--- workspaces/server/models/requests.go
+++ workspaces/server/models/requests.go
@@ -6,8 +6,8 @@ type CustomerRequest struct {
 	gorm.Model
 	RequestID   string `gorm:"primary_key" json:"requestID"`
 	EmployeeID  int64  `json:"employeeID"`
-	RoomID      int64  `gorm:"primary_key" json:"roomNumber"`
-	Email       string `gorm:"primary_key" json:"email"`
+	RoomID      int64  `json:"roomNumber"`
+	Email       string `json:"email"`
 	RequestType string `json:"requestType"`
 	Comment     string `json:"comment"`
 	Status      string `json:"status"`
