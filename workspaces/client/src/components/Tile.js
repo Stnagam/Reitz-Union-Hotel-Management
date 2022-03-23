f291ac2199f8c98fdc3f7c841aa820ed5d0201b7
hetanshpatel <hetanshnpatel@gmail.com>
2022-03-23 14:15:14 -0400

    Customer Request page

---
+++ workspaces/client/src/components/Tile.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { Button, Card } from "react-bootstrap";
+import Request from "./Request";
+import img from "../images/deluxe.jpg";
+
+const Tile = (props) => {
+  const [isRequested, setisRequested] = useState(false);
+  const onClick = (e) => {
+    // e.preventDefault();
+    if (isRequested == false) setisRequested(true);
+    if (isRequested == true) setisRequested(false);
+  };
+  return (
+    <div>
+      <Card >
+        <h4>Deluxe room</h4>
+        <img variant="top" s src={img} style={{ width: "80%" }} />
+        <Card.Body>
+          <p>Booking Date: 15-05-2022</p>
+
+          {props.isCurrent && (
+            <button type="submit" style={{"background-color":"#ffa500"}} onClick={onClick}>
+              Raise a reuqest
+            </button>
+          )}
+          {isRequested && <Request />}
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default Tile;
