3d558061b7eaee128cca5c9001379fbe8b004d35
sheelaippili <sheelaippili140@gmail.com>
2022-03-26 15:04:17 -0400

    updated bookings model with amount and reserved room variables

--- workspaces/server/models/bookings.go
+++ workspaces/server/models/bookings.go
@@ -1,16 +1,27 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type Booking struct {
 	gorm.Model
-	BookingID     string `gorm:"primary_key"  json:"bookingID"`
-	Email         string `json:"email"`
-	NoOfGuests    int32  `json:"noOfGuests"`
-	NoOfChildren  int32  `json:"noOfChildren"`
-	CheckIn       string `json:"checkin"`
-	CheckOut      string `json:"checkout"`
-	PaymentStatus string `json:"paymentStatus"`
+	BookingID     string    `gorm:"primary_key"  json:"bookingID"`
+	RoomID        uint      `gorm:"primary_key" json:"roomID"`
+	Email         string    `json:"email"`
+	NoOfGuests    int32     `json:"noOfGuests"`
+	NoOfChildren  int32     `json:"noOfChildren"`
+	CheckIn       time.Time `json:"checkin"`
+	CheckOut      time.Time `json:"checkout"`
+	Amount        string    `json:"amount"`
+	PaymentStatus string    `json:"paymentStatus"`
+	ReserveRooms  string    `gorm:"-" json:"reserveRooms"`
+}
+
+type RoomPrice struct {
+	NoOfRoomsToBook uint   `json:"noOfRoomsToBook"`
+	Amount          string `json:"amount"`
+	ReserveRooms    string `json:"reserveRooms"`
 }
