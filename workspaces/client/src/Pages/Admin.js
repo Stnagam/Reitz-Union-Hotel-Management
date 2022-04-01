12068b0e73cec8982fccde9b0bf7ba1087017361
hetanshpatel <hetanshnpatel@gmail.com>
2022-03-31 20:06:18 -0400

    Admin page

--- workspaces/client/src/Pages/Admin.js
+++ workspaces/client/src/Pages/Admin.js
@@ -4,33 +4,82 @@ import Footer from "../default/footer";
 import "../Style/admin.css";
 import { useState } from "react";
 import { Button, Card } from "react-bootstrap";
+import axios from "axios";
 
 const Admin = () => {
+  const [pendingData, setPendingData] = useState([]);
+  const [assignedData, setAssignedData] = useState([]);
+  const [completedData, setCompletedData] = useState([]);
+
   const [pending, setPending] = useState(false);
   const [assigned, setAssigned] = useState(false);
   const [completed, setCompleted] = useState(false);
   const [employee, setEmployee] = useState("");
+  // const [reqID, setReqID] = useState("");
+  let token = localStorage.getItem("token");
 
   const onClick1 = () => {
     setAssigned(false);
     setCompleted(false);
     setPending(true);
+    axios
+      .get("http://localhost:8080/auth/pendingReqs", {
+        headers: {
+          "x-access-token": token,
+        },
+      })
+      .then((res) => {
+        // let data = res.json();
+        setPendingData(res.data);
+        console.log(pendingData);
+      });
   };
   const onClick2 = () => {
     setCompleted(false);
     setPending(false);
     setAssigned(true);
+    axios
+      .get("http://localhost:8080/auth/assignedReqs", {
+        headers: {
+          "x-access-token": token,
+        },
+      })
+      .then((res) => {
+        setAssignedData(res.data);
+        console.log(assignedData);
+      });
   };
   const onClick3 = () => {
     setPending(false);
     setAssigned(false);
     setCompleted(true);
+    axios
+      .get("http://localhost:8080/auth/completedReqs", {
+        headers: {
+          "x-access-token": token,
+        },
+      })
+      .then((res) => {
+        setCompletedData(res.data);
+        console.log(completedData);
+      });
   };
 
   useEffect(() => {
     setAssigned(false);
     setCompleted(false);
     setPending(true);
+    axios
+      .get("http://localhost:8080/auth/pendingReqs", {
+        headers: {
+          "x-access-token": token,
+        },
+      })
+      .then((res) => {
+        // let data = res.json();
+        setPendingData(res.data);
+        console.log(pendingData);
+      });
   }, []);
 
   const [errors, seterrors] = useState([]);
@@ -46,26 +95,93 @@ const Admin = () => {
     seterrors(errors);
     return formIsValid;
   };
-  const onClick = () => {
+
+  const onAssign = (reqID) => {
     if (validation()) {
-      alert("Work is Assigned to " + { employee });
+      // console.log(employee);
+      // console.log(reqID);
+      axios.post(
+        "http://localhost:8080/auth/assignReqsToEmp",
+        {
+          requestID: reqID,
+          EmployeeID: employee,
+        },
+        {
+          headers: {
+            "x-access-token": token,
+          },
+        }
+      );
+      alert("Work is Assigned to " + JSON.stringify({ employee }));
+      axios
+        .get("http://localhost:8080/auth/pendingReqs", {
+          headers: {
+            "x-access-token": token,
+          },
+        })
+        .then((res) => {
+          setPendingData(res.data);
+        });
     }
   };
 
-  const Completed = () => {
+  const Completed = (reqID) => {
+    axios.post(
+      "http://localhost:8080/auth/markComplete",
+      {
+        requestID: reqID,
+      },
+      {
+        headers: {
+          "x-access-token": token,
+        },
+      }
+    );
+
     alert("Work is completed");
+    axios
+      .get("http://localhost:8080/auth/assignedReqs", {
+        headers: {
+          "x-access-token": token,
+        },
+      })
+      .then((res) => {
+        setAssignedData(res.data);
+      });
   };
 
-  const Deleted = () => {
-    alert("Request is delted from System");
+  const Deleted = (reqID) => {
+    axios.post(
+      "http://localhost:8080/auth/deleteCompleted",
+      {
+        requestID: reqID,
+      },
+      {
+        headers: {
+          "x-access-token": token,
+        },
+      }
+    );
+    alert("Request is deleted from System");
+    axios
+      .get("http://localhost:8080/auth/completedReqs", {
+        headers: {
+          "x-access-token": token,
+        },
+      })
+      .then((res) => {
+        setCompletedData(res.data);
+      });
   };
   return (
     <div className="admin">
       <Header_Common />
+
       <div className="navbar">
         <button className="button" onClick={onClick1}>
           Pending Request
         </button>
+
         <button className="button" onClick={onClick2}>
           Assigned Request
         </button>
@@ -74,90 +190,112 @@ const Admin = () => {
         </button>
       </div>
 
+      {/* <Pending pending={pending} pendingData={pendingData} />
+      <Assigned assigned={assigned} assignedData={assignedData} /> */}
       {pending && (
         <div>
           <h3>Pending Request</h3>
-          <Card>
-            <Card.Header>Room no: 101</Card.Header>
-            <Card.Body>
-              <Card.Title>Request type: Cleaning</Card.Title>
-              cmnt
-              <div>Status: Pending</div>
-              <div>
-                <input
-                  type="text"
-                  onChange={(e) => setEmployee(e.target.value)}
-                  placeholder="Assign employee"
-                />
-                <br/>
-                <button
-                  className="assign"
-                  variant="primary"
-                  onClick={onClick}
-                  style={{
-                    "background-color": "#00ab66",
-                    "margin-left": "30px",
-                  }}
-                >
-                  Assign work
-                </button>
-                <br />
-                <span style={{ color: "red", fontSize: "15px" }}>
-                  {errors["employee"]}
-                </span>
-              </div>
-            </Card.Body>
-          </Card>
+          {pendingData.map((pData) => {
+            const reqID = pData.requestID;
+
+            return (
+              <Card>
+                <Card.Header>Request id: {pData.requestID}</Card.Header>
+                <Card.Body>
+                  Room no: {pData.roomNumber}
+                  <Card.Title>Request type: {pData.requestType}</Card.Title>
+                  Comment: {pData.comment}
+                  <div>Status: Pending</div>
+                  <div>
+                    <input
+                      type="text"
+                      onChange={(e) => setEmployee(e.target.value)}
+                      placeholder="Assign employee"
+                    />
+                    <br />
+                    <button
+                      className="assign"
+                      variant="primary"
+                      onClick={() => onAssign(reqID)}
+                      style={{
+                        "background-color": "#00ab66",
+                        "margin-left": "70px",
+                      }}
+                    >
+                      Assign work
+                    </button>
+                    <br />
+                    <span style={{ color: "red", fontSize: "15px" }}>
+                      {errors["employee"]}
+                    </span>
+                  </div>
+                </Card.Body>
+              </Card>
+            );
+          })}
         </div>
       )}
+
       {assigned && (
         <div>
           <h3>Assigned Request</h3>
-          <Card>
-            <Card.Header>Room no: 101</Card.Header>
-            <Card.Body>
-              <Card.Title>Request type: Cleaning</Card.Title>
-              cmnt
-              <div>Status: Assigned</div>
-              <div>
-                Assigned to: Employee name
-                <br />
-                <button
-                  onClick={Completed}
-                  style={{
-                    "background-color": "#00ab66",
-                    "margin-left": "70px",
-                  }}
-                >
-                  Completed
-                </button>
-              </div>
-            </Card.Body>
-          </Card>
+          {assignedData.map((pData) => {
+            const reqID = pData.requestID;
+            return (
+              <Card>
+                <Card.Header>Request id: {pData.requestID}</Card.Header>
+                <Card.Body>
+                  Room no: {pData.roomNumber}
+                  <Card.Title>Request type: {pData.requestType}</Card.Title>
+                  Comment: {pData.comment}
+                  <div>Status: Assigned</div>
+                  <div>
+                    Assigned to: {pData.employeeID}
+                    <br />
+                    <button
+                      onClick={() => Completed(reqID)}
+                      style={{
+                        "background-color": "Yellow",
+                        "margin-left": "70px",
+                      }}
+                    >
+                      Completed
+                    </button>
+                  </div>
+                </Card.Body>
+              </Card>
+            );
+          })}
         </div>
       )}
       {completed && (
         <div>
           <h3>Completed Request</h3>
-          <Card>
-            <Card.Header>Room no: 101</Card.Header>
-            <Card.Body>
-              <Card.Title>Request type: Cleaning</Card.Title>
-              cmnt
-              <div>Status: Completed</div>
-              <div>Assigned to: Employee name</div>
-              <button
-                onClick={Deleted}
-                style={{
-                  "background-color": "Red",
-                  color: "white",
-                  "margin-left": "70px",
-                }}
-              >
-                Delete
-              </button>
-            </Card.Body>
-          </Card>
+          {completedData.map((pData) => {
+            const reqID = pData.requestID;
+            return (
+              <Card>
+                <Card.Header>RequestID: {reqID}</Card.Header>
+                Room no: 101
+                <Card.Body>
+                  <Card.Title>Request type: Cleaning</Card.Title>
+                  cmnt
+                  <div>Status: Completed</div>
+                  <div>Assigned to: Employee name</div>
+                  <button
+                    onClick={() => Deleted(reqID)}
+                    style={{
+                      "background-color": "Red",
+                      color: "white",
+                      "margin-left": "70px",
+                    }}
+                  >
+                    Delete
+                  </button>
+                </Card.Body>
+              </Card>
+            );
+          })}
         </div>
       )}
       <Footer />
