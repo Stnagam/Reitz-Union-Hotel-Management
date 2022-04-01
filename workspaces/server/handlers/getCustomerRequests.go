cc0e2b0bca5e9fc336fc37a8c6f609ac3647b2af
SAI TEJA NAGAM <47976398+Stnagam@users.noreply.github.com>
2022-04-01 19:27:39 -0400

    Resolved errorCheck bug

--- workspaces/server/handlers/getCustomerRequests.go
+++ workspaces/server/handlers/getCustomerRequests.go
@@ -8,51 +8,22 @@ import (
 )
 
 func GetPendingReqs(w http.ResponseWriter, r *http.Request) {
-	incomingCR := &models.CustomerRequest{}
-	err := json.NewDecoder(r.Body).Decode(incomingCR)
-	//check if a valid request has been sent from front end
-	if err != nil {
-		var resp = map[string]interface{}{"status": false, "message": "Invalid json request"}
-		json.NewEncoder(w).Encode(resp)
-		return
-	}
+
 	var allRequests []models.CustomerRequest
-
 	utils.DB.Where("status = ?", "Pending").Find(&allRequests)
-
 	json.NewEncoder(w).Encode(&allRequests)
 
 }
 
 func GetAssignedReqs(w http.ResponseWriter, r *http.Request) {
-	incomingCR := &models.CustomerRequest{}
-	err := json.NewDecoder(r.Body).Decode(incomingCR)
-	//check if a valid request has been sent from front end
-	if err != nil {
-		var resp = map[string]interface{}{"status": false, "message": "Invalid json request"}
-		json.NewEncoder(w).Encode(resp)
-		return
-	}
 	var allRequests []models.CustomerRequest
-
 	utils.DB.Where("status = ?", "Assigned").Find(&allRequests)
-
 	json.NewEncoder(w).Encode(&allRequests)
 
 }
 func GetCompletedReqs(w http.ResponseWriter, r *http.Request) {
-	incomingCR := &models.CustomerRequest{}
-	err := json.NewDecoder(r.Body).Decode(incomingCR)
-	//check if a valid request has been sent from front end
-	if err != nil {
-		var resp = map[string]interface{}{"status": false, "message": "Invalid json request"}
-		json.NewEncoder(w).Encode(resp)
-		return
-	}
 	var allRequests []models.CustomerRequest
-
 	utils.DB.Where("status = ?", "Completed").Find(&allRequests)
-
 	json.NewEncoder(w).Encode(&allRequests)
 
 }
