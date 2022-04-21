942100a7d2198c5d24e521cb16a32016c03dede5
SAI TEJA NAGAM <47976398+Stnagam@users.noreply.github.com>
2022-04-21 10:56:32 -0400

    Updated view_bookings.js

--- workspaces/client/cypress/integration/1-getting-started/view_bookings.js
+++ workspaces/client/cypress/integration/1-getting-started/view_bookings.js
@@ -1,22 +1,25 @@
-describe('Test', function () {
-  it('viewBooking', function () {
-    cy.visit('http://localhost:3000/');
-    cy.viewport('macbook-15');
-    cy.get('button').contains('Login').click();
+describe("Test", function () {
+  it("viewBooking", function () {
+    cy.visit("http://localhost:3000/");
+    cy.viewport("macbook-15");
+    cy.get("button").contains("Login").click();
     cy.get('input[name="username"]').type(name());
 
     cy.get('input[name="password"]').type(pass(8));
 
-    cy.get('button').contains('Login').click();
-    cy.visit('http://localhost:3000/customerHome');
+    cy.get("button").contains("Login").click();
+    cy.get(".button").click();
+    cy.get(".viewBooking").click();
+
+    // cy.viewport("macbook-15");  
   });
 
   function pass(i) {
-    const pass = '12345678';
+    const pass = "12345678";
     return pass;
   }
   function name() {
-    var text = 'he.patel@ufl.edu';
+    var text = "he.patel@ufl.edu";
     return text;
   }
 });
