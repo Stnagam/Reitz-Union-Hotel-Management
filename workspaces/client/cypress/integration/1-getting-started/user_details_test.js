8816ff1a72f634ba18ae56ebda32b6b18e9318e9
SAI TEJA NAGAM <47976398+Stnagam@users.noreply.github.com>
2022-04-21 10:54:40 -0400

    Updated user_details_test.js

--- workspaces/client/cypress/integration/1-getting-started/user_details_test.js
+++ workspaces/client/cypress/integration/1-getting-started/user_details_test.js
@@ -1,23 +1,23 @@
-describe('Test', function () {
-  it('viewProfile', function () {
-    cy.visit('http://localhost:3000/');
-    cy.viewport('macbook-15');
-    cy.get('button').contains('Login').click();
+describe("Test", function () {
+  it("viewProfile", function () {
+    cy.visit("http://localhost:3000/");
+    cy.viewport("macbook-15");
+    cy.get("button").contains("Login").click();
     cy.get('input[name="username"]').type(name());
-
     cy.get('input[name="password"]').type(pass(8));
-
-    cy.get('button').contains('Login').click();
-    // cy.get("Button[name='button']").click();
-    cy.visit('http://localhost:3000/profile');
+    cy.get("button").contains("Login").click();
+    cy.get(".button").click();
+    cy.get(".profile").click();
+      
+    cy.get(".button").click();
   });
 
   function pass(i) {
-    const pass = '12345678';
+    const pass = "12345678";
     return pass;
   }
   function name() {
-    var text = 'he.patel@ufl.edu';
+    var text = "he.patel@ufl.edu";
     return text;
   }
 });
